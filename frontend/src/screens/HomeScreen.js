a91f8377ad208cc3bf914e2e8934bfe18db58243
llk <872706089@qq.com>
2022-09-26 14:44:08 +0800

    get resource

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
-import products from './Products'
+import React, { useState, useEffect } from "react"
+import { Row, Col } from "react-bootstrap"
+import Product from "../components/Product"
+import axios from 'axios'
+// import products from "./Products"
 
 const HelloScreen = () => {
-  return <>
-    <Row>
-      {products.map((product) => (
-        <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-          <Product product={product}></Product>
-        </Col>
-      ))}
-    </Row>
-  </>;
-};
+  const [products, setProducts] = useState([])
 
-export default HelloScreen;
+  useEffect(() => {
+    // console.log('hello')
+    const fetchProducts = async () => {
+      const res = await axios.get('/api/products')
+      console.log(res)
+    }
+    fetchProducts()
+  })
+  return (
+    <>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product}></Product>
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HelloScreen
