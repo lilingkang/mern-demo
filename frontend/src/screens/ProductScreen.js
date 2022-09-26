88fc1137a50bff2afa7143bb207bd29ddece11f1
llk <872706089@qq.com>
2022-09-26 21:34:36 +0800

    connect database

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,13 +5,15 @@ import { useParams } from "react-router-dom"
 const ProductScreen = () => {
   const [product, setProduct] = useState({})
   const params = useParams() // 路由传参
-  const {id} = params
+  const { id } = params
   useEffect(() => {
     const fetchProduct = async () => {
       const { data } = await axios.get(`/api/products/${id}`)
+      // console.log(data)
       setProduct(data)
     }
     fetchProduct()
+    // console.log(product.name)
   }, [id])
   return <div>{product.name} 产品详情</div>
 }
