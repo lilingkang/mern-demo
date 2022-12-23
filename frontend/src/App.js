c578b961896d93f59f83f7721f46091754660e4e
lilingkang <872706089@qq.com>
2022-12-23 23:49:25 +0800

    测试api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
+  const [response, setResponse] = useState(1)
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.post('http://api.230123.cn/v1/c/ai_contents', {
+        token: 'IWN2MjwMWIEETQ',
+        title: '我要睡了'
+      })
+      setResponse(data)
+      console.log(data)
+    }
+    fetchData()
+  }, [])
   return (
     <>
       <h1>Hello, react!</h1>
-      
+      <p>code: { response.code }</p>
+      <p>msg: { response.msg }</p>
+      <p>data: { JSON.stringify(response.data) }</p>
+      <p>time: { new Date(response.time*1e3).toLocaleString() }</p>
     </>
   );
 }
